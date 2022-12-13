a311e109ee28323005b1cf14eea17fa4fb5b241f
Dylan <dylan.imail@gmail.com>
2022-12-13 16:49:24 -0500

    login working; form made

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,31 @@
 import { signIn, signOut, useSession } from 'next-auth/react';
 
-const LoginPanel = () => {
+const Form = () => {
+    return (
+        <form
+            className="flex"
+            onSubmit={(e) => {
+                e.preventDefault();
+            }}
+        >
+            <input
+                className="px-4 py-2 rounded-md border-2 border-zinc-800 bg-neutral-900 focus:outline-none"
+                type="text"
+                placeholder="Search"
+                minLength={2}
+                maxLength={100}
+            />
+            <button
+                type="submit"
+                className="p-2 rounded-md border-2 border-zinc-800 focus:outline-none"
+            >
+                Submit
+            </button>
+        </form>
+    );
+};
+
+const Home = () => {
     const { data: session, status } = useSession();
 
     if(status === "loading") {
@@ -8,12 +33,17 @@ const LoginPanel = () => {
     }
 
     return (
-        <>
+        <main className="flex flex-col items-center">
+            <h1 className="text-3xl pt-10">SearchTube</h1>
+            <p>A better way to consume content</p>
             <div className="pt-10">
                 {session ? (
                     <>
                         <p>Hi {session.user?.name}</p>
                         <button onClick={() => signOut()}>Logout</button>
+                        <div className="pt-6">
+                            <Form />
+                        </div>
                     </>
                 ) : (
                     <button onClick={() => signIn("discord")}>
@@ -21,16 +51,6 @@ const LoginPanel = () => {
                     </button>
                 )}
             </div>
-        </>
-    );
-};
-
-const Home = () => {
-    return (
-        <main className="flex flex-col items-center">
-            <h1 className="text-3xl pt-4">SearchTube</h1>
-            <p>A better way to consume content</p>
-            <LoginPanel />
         </main>
     );
 };
