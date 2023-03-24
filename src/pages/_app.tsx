4ba35bc54e8e4514e411cc786065e24fd7172e57
Dylan <dylan.imail@gmail.com>
2023-03-23 22:42:59 -0400

    page layout setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import NavBar from "../components/NavBar";
+import SideBar from "../components/SideBar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <div>
+                <NavBar />
+                <div className="h-full grid grid-cols-6">
+                    <SideBar />
+                    <main className="col-span-5 px-6">
+                        <Component {...pageProps} />
+                    </main>
+                </div>
+            </div>
+        </>
+    );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,32 +2,27 @@
 
 import { useState } from "react";
 import getYTSearchResults from "@/lib/youtubeSearch";
-import NavBar from "../components/NavBar";
-import SideBar from "../components/SideBar";
 
 const key = getYTSearchResults();
 console.log("key", process.env.YOUTUBE_API_KEY);
 
 export default function Home() {
-    const [searchValue, setSearchValue] = useState("");
     return (
         <div className="mt-4">
-            <NavBar />
-            <div className="h-full grid grid-cols-6">
-                <SideBar />
-                <main className="col-span-5 px-6">{children}</main>
-            </div>
             <SearchBar />
         </div>
     );
 }
 
 function SearchBar() {
+    const [searchValue, setSearchValue] = useState("");
     return (
         <div>
             <input
                 className="ml-2 p-1 rounded bg-neutral-900 border border-neutral-700"
                 type="text"
+                value={searchValue}
+                onChange={(event) => setSearchValue(event.target.value)}
             />
             <button className="ml-2 p-1 rounded border border-neutral-700">
                 Search
